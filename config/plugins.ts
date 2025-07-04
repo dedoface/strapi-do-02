b707daab4ef65414ccf1bf5fa4b375bda97526ff
Diego <diego@shakebiz.com>
2025-07-04 15:08:20 +0200

    changes credentials

--- config/plugins.ts
+++ config/plugins.ts
@@ -8,6 +8,10 @@ export default ({ env }: { env: (key: string, defaultValue?: string) => string }
             secretAccessKey: env('DO_SPACE_SECRET_KEY'),
             region: env('DO_SPACE_REGION'),
             endpoint: env('DO_SPACE_ENDPOINT'),
+            credentials: {
+              accessKeyId: env('DO_SPACE_ACCESS_KEY'),
+              secretAccessKey: env('DO_SPACE_SECRET_KEY'),
+            },
             params: {
               ACL: env('DO_SPACE_ACL', 'public-read'),
               Bucket: env('DO_SPACE_BUCKET'),
