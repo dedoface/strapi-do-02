34f96d2916bc9ae5b7beafdb568a849053dad333
Diego <diego@shakebiz.com>
2025-07-04 14:07:39 +0200

    do spaces provider

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,34 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'yourBucketName.yourRegion.cdn.digitaloceanspaces.com', // with CDN
+            'yourBucketName.yourRegion.digitaloceanspaces.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'yourBucketName.yourRegion.cdn.digitaloceanspaces.com', // with CDN
+            'yourBucketName.yourRegion.digitaloceanspaces.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,21 @@
-export default () => ({});
+export default ({ env }: { env: (key: string, defaultValue?: string) => string }) => ({
+    upload: {
+      config: {
+        provider: '@johngerome/strapi-provider-upload-spaces',
+        providerOptions: {
+          credentials: {
+            accessKeyId: env('DO_SPACE_ACCESS_KEY'),
+            secretAccessKey: env('DO_SPACE_SECRET_KEY'),
+          },
+          endpoint: env('DO_SPACE_ENDPOINT'),
+          region: env('DO_SPACE_REGION', 'us-east-1'),
+          bucket: env('DO_SPACE_BUCKET'),
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
+  });
