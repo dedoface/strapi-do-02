a1882035ce36ce3ade89f24466b01d6d42c50081
Diego <diego@shakebiz.com>
2025-07-04 14:50:47 +0200

    changes plugin ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,19 +1,17 @@
 export default ({ env }: { env: (key: string, defaultValue?: string) => string }) => ({
     upload: {
       config: {
-        provider: '@strapi/provider-upload-aws-s3',
+        provider: 'aws-s3',
         providerOptions: {
           s3Options: {
-            credentials: {
-              accessKeyId: env('DO_SPACE_ACCESS_KEY'),
-              secretAccessKey: env('DO_SPACE_SECRET_KEY'),
-            },
+            accessKeyId: env('DO_SPACE_ACCESS_KEY'),
+            secretAccessKey: env('DO_SPACE_SECRET_KEY'),
             region: env('DO_SPACE_REGION'),
             endpoint: env('DO_SPACE_ENDPOINT'),
-          },
-          params: {
-            ACL: env('DO_SPACE_ACL', 'public-read'),
-            Bucket: env('DO_SPACE_BUCKET'),
+            params: {
+              ACL: env('DO_SPACE_ACL', 'public-read'),
+              Bucket: env('DO_SPACE_BUCKET'),
+            },
           },
         },
         actionOptions: {
